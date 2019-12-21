937af33ab1ce7029778374c10c29b008fefb586b
Kevin Teague <kevin.t@waterbear.cloud>
2019-12-21 15:20:06 +0200

    specify config and prefix MYSQL_

--- deploy/config_app.sh
+++ deploy/config_app.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+CONFIG_FILE="/var/www/saas-app/aws.ini"
 DATABASE_ARN=`/tmp/get_rds_dsn.sh`
 
-sed -i -e "s/#DATABASE_ARN#/$DATABASE_ARN/" $CONFIG_FILE
+sed -i -e "s/#MYSQL_DATABASE_ARN#/$DATABASE_ARN/" $CONFIG_FILE
