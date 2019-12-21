7f694cd5604930f5ee9f5d3533dfe18ec99123c3
Kevin Teague <kevin.t@waterbear.cloud>
2019-12-21 11:33:52 +0200

    add an appspec.yml

---
+++ deploy/config_app.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+. /opt/aim/EC2Manager/ec2lm_functions.bash
+
+SECRETS_MANAGER_REF="${EC2LM_PACO_ENVIRONMENT_REF}.secrets_manager.saas.site"
+CONFIG_FILE="/var/www/saas-app/server.ini"
+
+ec2lm_replace_secret_in_file ${SECRETS_MANAGER_REF}.database "#MYSQL_DATABASE_PASSWORD#" ${CONFIG_FILE}
