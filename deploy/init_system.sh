80f68bf4f0435cc998a2b1bed2ed69a6527799b8
Kevin Teague <kevin.t@waterbear.cloud>
2019-12-22 16:52:02 +0200

    explicit path to saas-app in install

--- deploy/init_system.sh
+++ deploy/init_system.sh
@@ -5,7 +5,7 @@ echo "Installing Python virtualenv"
 BASE_DIR=/var/www/saas-app
 /bin/python3 -m venv $BASE_DIR/env
 $BASE_DIR/env/bin/pip install --upgrade pip setuptools
-$BASE_DIR/env/bin/pip install -e .
+$BASE_DIR/env/bin/pip install -e /var/www/saas-app/
 
 # database migrations directory
 mkdir -p $BASE_DIR/saas/alembic/versions
