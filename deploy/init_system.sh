ca68752bb5428d23dd0fb4eb105aa8c987c90962
Kevin Teague <kevin.t@waterbear.cloud>
2019-12-27 12:26:34 +0200

    fix db migration commands

--- deploy/init_system.sh
+++ deploy/init_system.sh
@@ -11,7 +11,10 @@ $BASE_DIR/env/bin/pip install -e /var/www/saas-app/
 mkdir -p $BASE_DIR/saas/alembic/versions
 
 # ToDo: create database
+# ToDo: only run this for db creation
+#$BASE_DIR/env/bin/initialize_saas_db /var/www/saas-app/aws.ini
+
 # run database migrations
-$BASE_DIR/env/bin/alembic -c development.ini revision --autogenerate -m "init"
-$BASE_DIR/env/bin/alembic -c development.ini upgrade head
-$BASE_DIR/env/bin/initialize_saas_db /var/www/saas-app/aws.ini
+$BASE_DIR/env/bin/alembic -c /var/www/saas-app/aws.ini revision --autogenerate -m "init"
+$BASE_DIR/env/bin/alembic -c /var/www/saas-app/aws.ini upgrade head
+
