92f637dcbef56fe4ba0fecbce08ef2e6745eff7b
Kevin Teague <kevin.t@waterbear.cloud>
2019-12-27 12:15:48 +0200

    comment on DSN replace command

--- deploy/config_app.sh
+++ deploy/config_app.sh
@@ -6,4 +6,5 @@ DATABASE_ARN=`/tmp/get_rds_dsn.sh`
 # escape any % characters as the .ini is Python interpolated when Pyramid loads it
 DATABASE_ARN=`sed 's/%/%%/g' <<< "$DATABASE_ARN"`
 
+# inject MySQL DSN into Pyramid config
 sed -i -e "s/#MYSQL_DATABASE_ARN#/$DATABASE_ARN/" $CONFIG_FILE
